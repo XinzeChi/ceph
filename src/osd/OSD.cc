df56c6bc8eb798883082269794f5dbcceb776978
Lu Shi <shi.lu@h3c.com>
2015-09-11 10:35:03 +0800

    osd:the fuction osd::shutdown Lock failed.
    Fixed:#13004
    
    Signed-off-by: Lu Shi <shi.lu@h3c.com>
    (cherry picked from commit f4498f527a1a7ddd53e38327a24c12095b1a0337)

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -6446,8 +6446,10 @@ void OSD::handle_osd_map(MOSDMap *m)
   else if (do_restart)
     start_boot();
 
+  osd_lock.Unlock();
   if (do_shutdown)
     shutdown();
+  osd_lock.Lock();
 
   m->put();
 }
